d5b13495485f372ed9db38ec5c48d080f0a4936e
Nícolas <107516938+nicofgr@users.noreply.github.com>
2024-10-04 12:56:33 -0300

    Update vimtex.lua

--- lua/plugin_config/vimtex.lua
+++ lua/plugin_config/vimtex.lua
@@ -4,6 +4,10 @@ return {
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
+    vim.g.tex_flavor = 'latex'
     vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_quickfix_mode = 0
+    vim.conceallevel = 1
+    vim.g.tex_conceal = 'abdmg'
   end
 }
