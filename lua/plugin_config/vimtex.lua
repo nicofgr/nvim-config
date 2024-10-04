7c73b523359f1bf0589ec303f5c7a2df5abba8e8
Nícolas <107516938+nicofgr@users.noreply.github.com>
2024-10-04 12:57:04 -0300

    Update vimtex.lua

--- lua/plugin_config/vimtex.lua
+++ lua/plugin_config/vimtex.lua
@@ -7,7 +7,7 @@ return {
     vim.g.tex_flavor = 'latex'
     vim.g.vimtex_view_method = "zathura"
     vim.g.vimtex_quickfix_mode = 0
-    vim.conceallevel = 1
+    vim.opt.conceallevel = 1
     vim.g.tex_conceal = 'abdmg'
   end
 }
