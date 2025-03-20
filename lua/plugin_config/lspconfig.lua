d778c9c6e75266acaf5f41eb84cfcb8b2b8e94c2
nicofgr <107516938+nicofgr@users.noreply.github.com>
2025-03-20 17:51:55 -0300

    Add lsp configs

--- lua/plugin_config/lspconfig.lua
+++ lua/plugin_config/lspconfig.lua
@@ -18,6 +18,8 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       require("lspconfig").clangd.setup{}
+      require("lspconfig").lua_ls.setup{}
+      require("lspconfig").texlab.setup{}
     end,
   },
 }
