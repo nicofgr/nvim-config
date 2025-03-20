d512d89adf640e1841f92c44531d2652d5c8583f
nicofgr <107516938+nicofgr@users.noreply.github.com>
2025-03-20 17:41:34 -0300

    Fix :mason not working

--- lua/plugin_config/lspconfig.lua
+++ lua/plugin_config/lspconfig.lua
@@ -1,15 +1,23 @@
 return {
   {
     "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-
     config = function()
       require("mason").setup()
-      require("mason-lspconfig").setup(){
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
         ensure_installed = { "clangd", "bashls", "cssls", "html", "texlab", "glsl_analyzer", "phpactor", "lua_ls"},
-      }
-
+        automatic_instalattion = true,
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("lspconfig").clangd.setup{}
     end,
   },
 }
