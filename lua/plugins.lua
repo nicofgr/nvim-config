8867682108116f0be37589a0656816387aa3b178
nicofgr <107516938+nicofgr@users.noreply.github.com>
2025-03-20 15:48:10 -0300

    Merge mason and lsp config files

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -23,9 +23,9 @@ require("lazy").setup({
     require 'plugin_config/telescope',
     require 'plugin_config/treesitter',  -- PL Parser
     require 'plugin_config/which-key',
-    require 'plugin_config/lspconfig',  -- Must be vefore Mason
-    require 'plugin_config/mason',
+    --require 'plugin_config/mason',
+    require 'plugin_config/lspconfig',  -- With Mason
     require 'plugin_config/LuaSnip',
-    require 'plugin_config/cmp',
+    require 'plugin_config/cmp',  -- Autocomplete
     require 'plugin_config/vimtex',
 })
