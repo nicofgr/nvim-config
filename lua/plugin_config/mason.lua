539c8746bdccf82d0cd12d696ba2690dc8b5187f
nicofgr <107516938+nicofgr@users.noreply.github.com>
2025-03-20 15:17:20 -0300

    Add Mason config

--- lua/plugin_config/mason.lua
+++ lua/plugin_config/mason.lua
@@ -1,5 +1,16 @@
 return {
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
+  {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup(){
+        ensure_installed = { "clangd", "bashls", "cssls", "html", "texlab", "glsl_analyzer", "phpactor", "lua_ls"},
+        automatic_installation = true,
+      }
+
+    end,
+  },
 }
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -19,12 +19,12 @@ require("lazy").setup({
   end,
 },
 
-    require 'plugin_config/lualine',
+    require 'plugin_config/lualine',  -- Line at bottom of screen
     require 'plugin_config/telescope',
-    require 'plugin_config/treesitter',
-     require 'plugin_config/which-key',
---     require 'plugin_config/mason',
-     require 'plugin_config/lspconfig',
+    require 'plugin_config/treesitter',  -- PL Parser
+    require 'plugin_config/which-key',
+    require 'plugin_config/lspconfig',  -- Must be vefore Mason
+    require 'plugin_config/mason',
     require 'plugin_config/LuaSnip',
     require 'plugin_config/cmp',
     require 'plugin_config/vimtex',
